c97c2f86af1f3f62acff84d358bf96479d6ca66e
sid-r-singh <68782815+sid-r-singh@users.noreply.github.com>
2021-03-07 12:04:54 +0530

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#git config user.name "sid-r-singh"
+#git config user.email "68782815+sid-r-singh@users.noreply.github.com"
+#git config user.email sid-r-singh@users.noreply.github.com  --also works
+#git pull
+#git add . 
+#git commit -m "Updated Repository data"
+#git push -u origin main:gh-pages
+#git checkout gh-pages -- repo_data.json
+
+echo "$INPUT_TRIALS"
