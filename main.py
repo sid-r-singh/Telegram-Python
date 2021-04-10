b57cc25b5a233b603a3192a4865220e31d7cf0a2
sid-r-singh <ofw3210@gmail.com>
2021-04-10 20:48:36 +0530

    Increase pi value accuracy to 13 digits

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ plt.locator_params(axis='y', nbins=5)
 x = np.array([0,0,3,3])
 y = np.array([10,40,40,10])
 textstr="""Program info
-Value of pi is:  %1.5f
+Value of pi is:  %1.13f
 No. of points: %s
 Exec. time:  %3.1fs"""%(pi,readable_trials,diff)
 plt.xticks([])
